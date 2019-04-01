5d721eaa4be3ea7c97585a8024963ffccb107c93
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-31 22:09:49 -0600

    description added

--- src/components/flexbox/FlexBox.js
+++ src/components/flexbox/FlexBox.js
@@ -14,18 +14,24 @@ export default class FlexBox extends PureComponent {
           <h2 className={styles.title}>Flex Box</h2>
         </header>
         <main className={styles.main}>
-            <select className={styles.select} onChange={this.handleChange}>
-                <option value='row'>Row</option>
-                <option value='row-reverse'>Row-Reverse</option>
-                <option value='column'>Column</option>
-                <option value='column-reverse'>Column-Reverse</option>
-            </select>
+            <div className={styles.flexDirection}>
+                <label className={styles.directionLabel}>Flex Direction</label>
+                <select className={styles.select} onChange={this.handleChange}>
+                    <option value='row'>Row</option>
+                    <option value='row-reverse'>Row-Reverse</option>
+                    <option value='column'>Column</option>
+                    <option value='column-reverse'>Column-Reverse</option>
+                </select>
+            </div>
             <div className={styles.boxContainer} name='boxContainer'>
                 <div className={styles.one}>1</div>
                 <div className={styles.two}>2</div>
                 <div className={styles.three}>3</div>
                 <div className={styles.four}>4</div>
             </div>
+            <p className={styles.description}>
+                Flex box is everyting you need to position elements how you&#39;d like. Play with some of the above controls to move these four boxes around.
+            </p>
         </main>
       </>
     )
