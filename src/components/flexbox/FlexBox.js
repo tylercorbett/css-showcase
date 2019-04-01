5b7eeac8d8206f1a0c3470703e7e6d26329daf1f
Tyler Corbett <thetylercorbett@gmail.com>
2019-03-31 21:48:22 -0600

    basic flexbox demo set up

--- src/components/flexbox/FlexBox.js
+++ src/components/flexbox/FlexBox.js
@@ -5,6 +5,13 @@ export default class FlexBox extends PureComponent {
   state = {
     flexDirection: 'row'
   }
+  handleChange = event => {
+    console.log(event.target.value);
+    const value = event.target.value;
+    const boxContainer = document.getElementsByName('boxContainer')[0];
+    console.log(boxContainer);
+    boxContainer.style.flexDirection = (value);
+  }
   render() {
     return(
       <>
@@ -12,13 +19,13 @@ export default class FlexBox extends PureComponent {
           <h2 className={styles.title}>Flex Box</h2>
         </header>
         <main className={styles.main}>
-            <select className={styles.select}>
+            <select className={styles.select} onChange={this.handleChange}>
                 <option value='row'>Row</option>
-                <option value='rowReverse'>Row-Reverse</option>
+                <option value='row-reverse'>Row-Reverse</option>
                 <option value='column'>Column</option>
-                <option value='columnReverse'>Column-Reverse</option>
+                <option value='column-reverse'>Column-Reverse</option>
             </select>
-            <div className={styles.boxContainer}>
+            <div className={styles.boxContainer} name='boxContainer'>
                 <div className={styles.one}>1</div>
                 <div className={styles.two}>2</div>
                 <div className={styles.three}>3</div>
