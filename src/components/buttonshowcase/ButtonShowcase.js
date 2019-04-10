b65fdd399b13d2d4e6c5757c669fbfeb3811b272
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-10 10:56:50 -0700

    next styles checkpoint

--- src/components/buttonshowcase/ButtonShowcase.js
+++ src/components/buttonshowcase/ButtonShowcase.js
@@ -8,14 +8,25 @@ export default class ButtonShowcase extends PureComponent {
         <header>
           <h2 className={styles.title}>Button Builder</h2>
         </header>
-        <div className={styles.controlsContainer}>
-          <select className={styles.select}>
-            <option name='round' value='round'>Round</option>
-            <option name='square' value='square'>Square</option>
-            <option name='circle' value='circle'>Circle</option>
-          </select>
-          <input className={styles.color} type='color'/>
-        </div>
+        <main>
+          <div className={styles.controlsContainer}>
+            <div className={styles.shapeContainer}>
+              <h3 className={styles.h3}>Shape</h3>
+              <select className={styles.select}>
+                <option name='square' value='square'>Square</option>
+                <option name='round' value='round'>Round</option>
+                <option name='circle' value='circle'>Circle</option>
+              </select>
+            </div>
+            <div className={styles.colorContainer}>
+              <h3 className={styles.h3}>Color</h3>
+              <input className={styles.color} type='color'/>
+            </div>
+          </div>
+          <div className={styles.buttonContainer}>
+            <button className={styles.myButton}>Copy Styles</button>
+          </div>
+        </main>
       </>
     )
   }
