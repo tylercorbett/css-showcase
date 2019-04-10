d4cb566893bf5f46a5321659f1009333189a0407
Tyler Corbett <thetylercorbett@gmail.com>
2019-04-10 11:14:33 -0700

    DDD achieved

--- src/components/buttonshowcase/ButtonShowcase.js
+++ src/components/buttonshowcase/ButtonShowcase.js
@@ -8,19 +8,27 @@ export default class ButtonShowcase extends PureComponent {
         <header>
           <h2 className={styles.title}>Button Builder</h2>
         </header>
-        <main>
+        <main className={styles.main}>
           <div className={styles.controlsContainer}>
             <div className={styles.shapeContainer}>
               <h3 className={styles.h3}>Shape</h3>
               <select className={styles.select}>
                 <option name='square' value='square'>Square</option>
-                <option name='round' value='round'>Round</option>
+                <option name='rounded' value='rounded'>Rounded</option>
                 <option name='circle' value='circle'>Circle</option>
               </select>
             </div>
             <div className={styles.colorContainer}>
-              <h3 className={styles.h3}>Color</h3>
-              <input className={styles.color} type='color'/>
+              <h3 className={styles.h3}>Font Size</h3>
+              <input className={styles.backgroundColor} type='range' name='font-size'/>
+            </div>
+            <div className={styles.colorContainer}>
+              <h3 className={styles.h3}>Background Color</h3>
+              <input className={styles.backgroundColor} type='color'/>
+            </div>
+            <div className={styles.colorContainer}>
+              <h3 className={styles.h3}>Font Color</h3>
+              <input className={styles.backgroundColor} type='color'/>
             </div>
           </div>
           <div className={styles.buttonContainer}>
